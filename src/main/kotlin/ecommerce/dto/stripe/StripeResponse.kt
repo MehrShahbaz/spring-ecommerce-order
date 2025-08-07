6a19d202840eb867a055f7a779867f0a8b448279
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-08-07 17:44:30 +0200

    refactor(StripeResponse)

--- src/main/kotlin/ecommerce/dto/stripe/StripeResponse.kt
+++ src/main/kotlin/ecommerce/dto/stripe/StripeResponse.kt
@@ -3,7 +3,7 @@ package ecommerce.dto.stripe
 class StripeResponse(
     val id: String,
     val amount: Int,
-    val client_secret: String,
+    val status: String,
     val created: Int,
     val currency: String,
 )
