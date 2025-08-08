eda521e74b10f8960117a6e06d33a433f3b878b8
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-08-08 11:43:10 +0200

    refactor(WebConfig)

--- src/main/kotlin/ecommerce/config/WebConfig.kt
+++ src/main/kotlin/ecommerce/config/WebConfig.kt
@@ -17,7 +17,7 @@ class WebConfig(
 ) : WebMvcConfigurer {
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(authInterceptor)
-            .addPathPatterns("/api/member/cart/**", "/api/member/wish-list/**")
+            .addPathPatterns("/api/member/cart/**", "/api/member/order/**")
         registry.addInterceptor(adminInterceptor)
             .addPathPatterns("/api/admin/products/**", "/api/admin/cart_statistics/**")
         super.addInterceptors(registry)
